ca0bcd3f585c331b9296d5fe0942ecb2a81e62e4
Aleksandr Harchenko <morontt@gmail.com>
2014-12-13 23:56:55 +0200

    change file extension

--- ubuntu/backup/mysql_backup.sh
+++ ubuntu/backup/mysql_backup.sh
@@ -6,5 +6,5 @@ currentdate=`date '+%Y%m%d'`
 
 for database in $databases; do
     dumpname=$currentdate"_"$database
-    mysqldump -h localhost -u dbuser --password=dbpassword $database | gzip > /path/to/backup/$dumpname.gz
+    mysqldump -h localhost -u dbuser --password=dbpassword $database | gzip > /path/to/backup/$dumpname.sql.gz
 done
